903eb6416fac071d5e91188c66c636e8b7096977
tddrrr <teodora.birliba@gmail.com>
2021-01-21 17:57:38 +0200

    added react template

---
+++ front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React, {Component} from 'react'
+
+class App extends Component{
+  render() {
+    return(
+      <div className="App"></div>
+    )
+  };
+}
+
+export default App;
