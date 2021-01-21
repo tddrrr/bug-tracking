d1ec4605fabf66b4d6adcd8f7bff385c666137bd
tddrrr <teodora.birliba@gmail.com>
2021-01-22 00:30:36 +0200

    started login and register

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import React, {Component} from 'react'
+import Register from './components/Register'
+import Login from './components/Login'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+
 
 class App extends Component{
   render() {
     return(
-      <div className="App"></div>
+      <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Login}></Route>
+        <Route path="/register" exact component={Register}></Route>
+      </Switch>
+      </BrowserRouter>
     )
   };
 }
